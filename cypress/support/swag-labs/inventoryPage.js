8bf0c7aef063d833eba572eb99e73fb4455a1ba2
Predrag Damjanovic <115583804+predragdamjanovic@users.noreply.github.com>
2024-04-26 16:19:25 +0200

    Add files via upload

---
+++ cypress/support/swag-labs/inventoryPage.js
@@ -0,0 +1,20 @@
+class Inventory{
+
+    getBackpack(){
+        return cy.get('[data-test="item-4-title-link"] > [data-test="inventory-item-name"]');
+    }
+
+    checkBackpack(proizvod){
+        this.getBackpack().should("have.text", proizvod);
+    }
+
+    clickBackpack(){
+        this.getBackpack().click();
+    }
+
+    
+
+}
+
+
+export default Inventory;
