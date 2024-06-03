503b4ef31c98c45628cc1cc9ace2288a8a0bea71
nikolicl99 <lnikolic381@gmail.com>
2024-06-04 01:01:34 +0200

    Update loginPage.js

--- cypress/support/qase/loginPage.js
+++ cypress/support/qase/loginPage.js
@@ -1,42 +1,30 @@
-class LoginPage {
-    loginPageTitle() {
-      return cy.get('.NsFoEI');
-    }
-  
-    verifyUserIsOnLoginPage() {
-      this.loginPageTitle().should('be.visible');
-      this.loginPageTitle().contains('Log in to your account');
-    }
-  
-    emailField() {
-      return cy.get('input[name="email"]');
-    }
-  
-    passwordField() {
-      return cy.get('input[name="password"]');
-    }
-  
-    loginButton() {
-      return cy.get('button[type="submit"] > span.CAunhU');
-    }
-  
-    enterCredsAndLogin() {
-      const email = Cypress.env('qase_email');
-      const password = Cypress.env('qase_password');
-  
-      // Debugging statements
-      console.log('Email:', email);
-      console.log('Password:', password);
-  
-      if (!email || !password) {
-        throw new Error('Email or password is not set in Cypress environment variables');
-      }
-  
-      this.emailField().type(email);
-      this.passwordField().type(password);
-      this.loginButton().click();
-    }
+class LoginPage{
+  loginPageTitle(){
+    return cy.get('.NsFoEI');
   }
-  
-  export default LoginPage;
-  
+
+  verifyUserIsOnLoginPage(){
+    this.loginPageTitle().should('be.visible');
+    this.loginPageTitle().contains('Log in to your account');
+  }
+
+  emailField() {
+    return cy.get('input[name="email"]');
+  }
+
+  passwordField() {
+    return cy.get('input[name="password"]');
+  }
+
+  loginButton() {
+    return cy.get('button[type="submit"] > span.CAunhU');
+  }
+
+  enterCredsAndLogin() {
+    this.emailField().type(Cypress.env('qase_email'))
+    this.passwordField().type(Cypress.env('qase_password'))
+    this.loginButton().click()
+  }
+}
+
+export default LoginPage;
