5175443a818083626d41320db12ad090517f7c12
nikola2909 <nlkolaa@icloud.com>
2024-06-05 18:32:43 +0200

    Domaci 3
    Odradjen domaci 3

--- cypress/e2e/Qase/qase.js
+++ cypress/e2e/Qase/qase.js
@@ -2,72 +2,65 @@ import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 import LoginPage from "../../support/qase/loginPage";
 import HomePage from "../../support/qase/homePage";
 
-const loginPage = new LoginPage();
+const loginPageQase = new LoginPage();
 const homePage = new HomePage();
 var projectNameDef = null;
+const username = "qaseqase907@gmail.com";
+const password = "Testiranje1!"
+
 
 Given("User is on Quase Login page", () => {
   cy.visit("https://app.qase.io/login");
-  loginPage.verifyUserIsOnLoginPage();
+  loginPageQase.verifyUserIsOnLoginPage();
+
+
+  When("User enters his credentials and clicks login button", () => {
+      loginPageQase.login(username, password);
+  })
+
+  Then("User shoud see Quase home page", ()=> {
+      cy.get('.mYdffk').should("exist");
+      cy.get('.sFz8ki > :nth-child(2)').should("exist");
+      cy.get('[href="/workspace"]').should("exist");
+      cy.get('[href="/report/dashboards"]').should("exist");
+      cy.get('[href="/report/queries-list"]').should("exist");
+      cy.get('[data-feature="true"]').should("exist");
+  })
+
 })
 
 Given("User is logged in on Quase application", () => {
   cy.visit("https://app.qase.io/login");
-  loginPage.verifyUserIsOnLoginPage();
-  loginPage.enterCredsAndLogin();
+  loginPageQase.verifyUserIsOnLoginPage();
+  loginPageQase.login(username, password);
   homePage.checkIfTitleIsPresent();
-})
 
-When("User enters his credentials and clicks login button", () => {
-  loginPage.enterCredsAndLogin();
-})
 
-When("User creates new project with following details", (dataTable) => {
-  var data = dataTable.hashes();
-  var projectName = data[0].projectName;
-  var projectCode = data[0].projectCode;
-  var projectDescription = data[0].projectDescription;
-  projectNameDef = projectName;
-
-  homePage.createNewProject(projectName, projectCode, projectDescription);
-})
-
-Then("User shoud see Quase home page", () => {
-  homePage.checkIfTitleIsPresent();
-})
-
-Then("User should be able to see new project on home page", () => {
-  homePage.verifyNewProjectIsCreated(projectNameDef);
-})
-
-Then("User is able to delete created project", () => {
-  homePage.projectOptionsDropdown().click();
-  homePage.deleteOptionInDropdown().click();
-  homePage.deleteModal().should('be.visible')
-  homePage.deleteModal().contains(projectNameDef)
-  homePage.confirmProjectDeleteButton().click();
-})
-
-And("User returns to Home Page", () => {
-  homePage.returnToHomePage();
-})
-
-And("User does not see the project in the table", () => {
-  cy.get('tbody tr').should('have.length', 1);
-})
-
-When("User wants to update project", () => {
-  homePage.projectOptionsDropdown().click();
-  homePage.settingsOptionInDropdown().click();
-})
-
-And("User change the logo", () => {
-  // cy.get('.YAFTyj').selectFile('cypress/fixtures/logo.png')
-  cy.get('input[type=file]').selectFile('cypress/fixtures/logo.png', { force: true })
-})
-
-Then("update shoul be successfull", () => {
-  cy.contains('Project avatar was successfully updated!')
-  cy.contains('Update settings').click()
-  homePage.returnToHomePage();
+  When("User creates new project", () => {
+      homePage.createNewProject();
+    })
+    
+    Then("User shoud see Quase home page", () => {
+      homePage.checkIfTitleIsPresent();
+    })
+    
+    Then("User should be able to see new project on home page", () => {
+      homePage.verifyNewProjectIsCreated();
+    })
+    
+    Then("User is able to delete created project", () => {
+      homePage.projectOptionsDropdown().click();
+      homePage.deleteOptionInDropdown().click();
+      homePage.deleteModal().should('be.visible')
+      homePage.deleteModal().contains('Testiranje')
+      homePage.confirmProjectDeleteButton().click();
+    })
+    
+    And("User returns to Home Page", () => {
+      homePage.returnToHomePage();
+    })
+    
+    And("User does not see the project in the table", () => {
+      cy.get('tbody tr').should('have.length', 1);
+    })
 })
--- cypress/support/qase/loginPage.js
+++ cypress/support/qase/loginPage.js
@@ -1,30 +1,28 @@
 class LoginPage{
   loginPageTitle(){
     return cy.get('.NsFoEI');
-  }
+}
 
-  verifyUserIsOnLoginPage(){
+verifyUserIsOnLoginPage(){
     this.loginPageTitle().should('be.visible');
     this.loginPageTitle().contains('Log in to your account');
-  }
+}
 
-  emailField() {
-    return cy.get('input[name="email"]');
-  }
+usernameField() {
+    return cy.get(':nth-child(1) > .iUgi_m > .wBsjbV');
+}
+passwordField() {
+    return cy.get(':nth-child(2) > .iUgi_m > .wBsjbV');
+}
+loginButton() {
+    return cy.get('.Ipyboe > .CAunhU');
+}
 
-  passwordField() {
-    return cy.get('input[name="password"]');
-  }
-
-  loginButton() {
-    return cy.get('button[type="submit"] > span.CAunhU');
-  }
-
-  enterCredsAndLogin() {
-    this.emailField().type(Cypress.env('qase_email'))
-    this.passwordField().type(Cypress.env('qase_password'))
+login(username, password) {
+    this.usernameField().type(username);
+    this.passwordField().type(password);
     this.loginButton().click()
-  }
+}
 }
 
 export default LoginPage;
